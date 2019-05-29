5d5e90400dabf79787aaf5fafb94a049f94d6595
Laurynas <laurynas.kr@hotmail.com>
2019-05-29 21:50:53 +0300

    js and working prototype

---
+++ functions.js
@@ -0,0 +1,47 @@
+function getWords(clback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            clback(this.responseText);
+        }
+    };
+    xhttp.open("GET", "control.php?command=1", true);
+    xhttp.send();
+}
+
+function checkObj(obj) {
+    if (obj.phrase != "OK") {
+        return false;
+    }
+    
+    if (obj.length < 10) {
+        return false;
+    }
+    
+    if (!obj.combination) {
+        return false;
+    }
+    
+    return true;
+}
+
+function getInsult(combination) {
+    word_array = combination.split(",");
+    
+    if (word_array.length != 3) {
+        return false;
+    }
+    
+    insult = word_array[0] + " " + word_array[1] + ", " + word_array[2];
+    return insult;
+}
+
+function clback(json_txt) {
+    console.log(json_txt);
+    json_obj = JSON.parse(json_txt);
+    if (!checkObj(json_obj)) {
+        return false;
+    }
+    text = getInsult(json_obj.combination);
+    document.getElementById("vieta").innerHTML = text;
+}
--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+<script type="text/javascript" src="functions.js"></script>
+<script type="text/javascript">
+
+</script>
 </head>
 
+
 <body>
+<div id="vieta"></div>
+
+<button type="button" onclick="getWords(clback);">lil</button>
+
 </body>
 
 </html>
