682aaa8953a2df12a4481c1f3d88583dd1528f97
Climbman <laurynas.kr@hotmail.com>
2019-06-20 22:05:33 +0300

    removed trash code from control.php

--- control.php
+++ control.php
@@ -10,28 +10,26 @@ class ResponseObject {
 	}
 }
 
-
-$len1 = count($_DICT1);
-$len2 = count($_DICT2);
-$len3 = count($_DICT3);
-
-$rand1 = rand(0, $len1 - 1);
-$rand2 = rand(0, $len2 - 1);
-$rand3 = rand(0, $len3 - 1);
-
 $_GET["command"] = 1;
 
 switch ($_GET["command"]) {
-    case 0:
-        echo $_DICT1[$rand1] . " " . $_DICT2[$rand2] . ", " . $_DICT3[$rand3];
-        break;
-    default:
-        echo null;
-        break;
     case 1:
+        $len1 = count($_DICT1);
+        $len2 = count($_DICT2);
+        $len3 = count($_DICT3);
+
+        $rand1 = rand(0, $len1 - 1);
+        $rand2 = rand(0, $len2 - 1);
+        $rand3 = rand(0, $len3 - 1);
+        
 		$obj = New ResponseObject($_DICT1[$rand1], $_DICT2[$rand2], $_DICT3[$rand3]);
 		$response_json = json_encode($obj);
 		echo $response_json;
 		break;
+        
+        
+    default:
+        echo null;
+        break;
 }
 ?>
